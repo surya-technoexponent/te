87ffa38374972c5b3fc15a89fa8a97e7ae4878bd
surya-technoexponent <133856479+surya-technoexponent@users.noreply.github.com>
2023-07-06 18:42:08 +0530

    Code cleaning and Bug Fixes

--- scripts/widget.js
+++ scripts/widget.js
@@ -1,11 +1,7 @@
-const Widget = new TradingView()
-Widget.widget(
-    {
-    "width": 720,
-    "height": 240,
-    "symbol": "COINBASE:BTCUSD",
-    "interval": "3",
-    "timezone": "Etc/UTC",
+const widgetConfig = {
+  "autosize": true,
+    "interval": "1D",
+    "timezone": "America/New_York",
     "theme": "dark",
     "style": "1",
     "locale": "en",
@@ -15,6 +11,62 @@ Widget.widget(
     "hide_legend": true,
     "allow_symbol_change": true,
     "save_image": false,
-    "container_id": "tradingview_e2091"
+    "backgroundColor": "rgba(0, 0, 0, 1)",
+}
+
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "TVC:DXY",
+    "container_id": "widget-area-01"
   }
-    );
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "AMEX:DIA",
+    "container_id": "widget-area-02"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "TVC:GOLD",
+    "container_id": "widget-area-03"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "NYMEX:CL1!",
+    "container_id": "widget-area-04"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "CRYPTO:BTCUSD",
+    "container_id": "widget-area-05"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "NASDAQ:QQQ",
+    "container_id": "widget-area-06"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "AMEX:SPY",
+    "container_id": "widget-area-07"
+  }
+);
+new TradingView.widget(
+  {
+    ...widgetConfig,
+    "symbol": "AMEX:IWM",
+    "container_id": "widget-area-08"
+  }
+);
